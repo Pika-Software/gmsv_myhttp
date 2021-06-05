32da2f10061bc5ea1513b804422eae27f0c62a2b
Retro <44505837+dankmolot@users.noreply.github.com>
2021-06-05 09:49:41 +0300

    add premake5 contrib libraries

--- premake5.lua
+++ premake5.lua
@@ -3,27 +3,7 @@ include('third-party/garrysmod_common')
 
 CreateWorkspace({ name = "myhttp", abi_compatible = false })
     group "Third Party"
-        project "curl"
-            kind "StaticLib"
-            language "C"
-            defines     { "BUILDING_LIBCURL", "CURL_STATICLIB", "HTTP_ONLY" }
-            warnings    "off"
-
-            includedirs{ 'third-party/curl/include', 'third-party/curl/lib' }
-            files { 'third-party/curl/lib/**.c', 'third-party/curl/lib/**.h' }
-
-            filter { "system:windows" }
-                defines { "USE_SCHANNEL", "USE_WINDOWS_SSPI" }
-                links "crypt32"
-
-            filter { "system:macosx" }
-                defines { "USE_DARWINSSL" }
-
-            filter { "system:not windows", "system:not macosx" }
-                defines { "USE_MBEDTLS" }
-
-            filter { "system:linux or bsd or solaris" }
-                defines { "CURL_HIDDEN_SYMBOLS" }
+        include('third-party/curl')
 
     CreateProject({ serverside = true })
         IncludeHelpersExtended()
@@ -34,5 +14,6 @@ CreateWorkspace({ name = "myhttp", abi_compatible = false })
         files {"src/*.cpp", "src/*.hpp"}
 
         -- third-party libraries
-        links { 'curl' }
+        defines { "CURL_STATICLIB"}
+        links { 'curl-lib' }
         sysincludedirs{ 'third-party/curl/include' }
